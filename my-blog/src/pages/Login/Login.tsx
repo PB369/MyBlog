a17d88b9aec1424768351d9e12f82dbc04555a23
PB369 <pedrohbarros369@gmail.com>
2025-04-05 15:06:19 -0300

    Login page content applied

--- my-blog/src/pages/Login/Login.tsx
+++ my-blog/src/pages/Login/Login.tsx
@@ -6,9 +6,15 @@ const Login = () => {
   return (
     <>
       <Header/>
-      <div>
-        <h2>Tela de Login</h2>
-      </div>
+      <form action="*" method='post'>
+        <h2>Administrator Login</h2>
+        <label htmlFor="usernameInput">Username:</label>
+        <input type="text" id="usernameInput"/>
+        <label htmlFor="passwordInput">Password:</label>
+        <input type="text" id="passwordInput"/>
+        <button type="submit">Access</button>
+        {/* <p>Username or password is invalid</p> */}
+      </form>
       <Footer/>
     </>
   )
