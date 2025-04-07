57fcfbf3b8a1d3cc3e8cdc1cf885e2f4579e7130
PB369 <pedrohbarros369@gmail.com>
2025-04-07 18:40:25 -0300

    StaticArticleCard styling under development

--- my-blog/src/components/HomeMain/HomeMain.tsx
+++ my-blog/src/components/HomeMain/HomeMain.tsx
@@ -1,19 +1,22 @@
 
 import { ReactNode } from 'react';
-import './css/HomeMain.module.scss';
+import styles from './css/HomeMain.module.scss';
+import { useTheme } from '../../context/ThemeContext';
 
 type Props = {
   children: ReactNode;
 }
 
 const HomeMain = ({children} : Props) => {
+  const { theme } = useTheme();
+
   return (
-    <>
+    <div className={styles[theme]}>
       <h2>Articles for fun</h2>
       <section>
         {children}
       </section>
-    </>
+    </div>
   )
 }
 
--- my-blog/src/components/StaticArticleCard/StaticArticleCard.tsx
+++ my-blog/src/components/StaticArticleCard/StaticArticleCard.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
-import './css/StaticArticleCard.module.scss';
+import styles from './css/StaticArticleCard.module.scss';
+import { useTheme } from '../../context/ThemeContext';
 
 type Props = {
   id: number | undefined,
@@ -12,19 +13,21 @@ type Props = {
 }
 
 const StaticArticleCard = ({id, title, tags, publishDate, bannerURL, bannerAlt, content}: Props) => {
+  const { theme } = useTheme();
+
   return (
     <>
-      <article>
-        <div>
-          <div>
-            {tags.map(tag => <span>{tag}</span>)}
-            <div>
+      <article className={`${styles.article} ${styles[theme]}`}>
+        <div className={styles.articleTextualContent}>
+          <div className={styles.articleMainContent}>
+            <div className={styles.tagsContainer}>{tags.map(tag => <span className={styles.tags}>{tag}</span>)}</div>
+            <div className={styles.articleHead}>
               <h4>{title}</h4>
               <p>{publishDate}</p>
             </div>
             <p>{content}</p>
           </div>
-          <Link to={`/articles/${typeof(id) === "number" ? id.toString() : undefined}`}>Read it all</Link>
+          <Link to={`/articles/${typeof(id) === "number" ? id.toString() : undefined}`} className={styles.link}>Read it all</Link>
         </div>
         <img src={bannerURL} alt={bannerAlt}/>
       </article>
