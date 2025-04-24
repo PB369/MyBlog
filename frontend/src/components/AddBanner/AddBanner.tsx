3cf503478dd7c3c74628955f8bee580148c5a868
PB369 <pedrohbarros369@gmail.com>
2025-04-24 11:42:23 -0300

    Add banner functionality under development

--- frontend/src/components/AddBanner/AddBanner.tsx
+++ frontend/src/components/AddBanner/AddBanner.tsx
@@ -1,5 +1,6 @@
 import styles from './css/AddBanner.module.scss'
 import blackPlusIcon from '../../../public/OtherIcons/blackPlus-icon.png'
+import { useRef, useState } from 'react'
 
 type Props = {
     isNewArticle: boolean,
@@ -8,10 +9,26 @@ type Props = {
 }
 
 const AddBanner = ({isNewArticle, banner_url, banner_alt}: Props) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+    const [bannerURL, setBannerURL] = useState(banner_url);
+
+    const handleBtnClick = () => {
+        inputRef.current?.click()
+    }
+
+    const handleBannerChoice = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const image = event.target.files?.[0]
+        if(image){
+            console.log(image);
+            banner_url=URL.createObjectURL(image);
+            setBannerURL(banner_url)
+        }
+    }
+
     return (
         <>
-            <button className={styles.addBannerBtn}>
-                <img src={isNewArticle ? undefined : banner_url} alt={isNewArticle ? "" :banner_alt} className={styles.banner}/>
+            <button className={styles.addBannerBtn} onClick={handleBtnClick}>
+                <img src={bannerURL} alt={isNewArticle ? "" :banner_alt} className={styles.banner}/>
                 <div className={styles.addDivLabel}>
                     <div className={styles.plusIcon}>
                         <img src={blackPlusIcon} alt="plus-icon" />
@@ -19,6 +36,7 @@ const AddBanner = ({isNewArticle, banner_url, banner_alt}: Props) => {
                     <p className={styles.addText}>Add an image</p>
                 </div>
             </button>
+            <input type="file" accept='image/*' ref={inputRef} style={{display:"none"}} onChange={handleBannerChoice} />
         </>
     )
 }
