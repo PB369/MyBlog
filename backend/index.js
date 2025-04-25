a66bf7994bfad34b5147b581ba026331d8d35342
PB369 <pedrohbarros369@gmail.com>
2025-04-25 00:57:39 -0300

    Better configuration to Allow-Origin header is applied

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,13 @@ const articleModel = require("./articleModel");
 
 app.use(express.json());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  const allowedOrigins = ["http://localhost:5173", "https://pb369-projects-myblog.vercel.app"]
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers");
   next();
