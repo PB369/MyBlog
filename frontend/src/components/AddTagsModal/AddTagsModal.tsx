1b66a45df9cdd7dd30df1467a9b88853f947d70f
PB369 <pedrohbarros369@gmail.com>
2025-05-04 14:58:13 -0300

    AddTagsModal first version of add and remove tags features is finished. Still need to be improved.

--- frontend/src/components/AddTagsModal/AddTagsModal.tsx
+++ frontend/src/components/AddTagsModal/AddTagsModal.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import ErrorMessage, { Errors } from '../ErrorMessage/ErrorMessage';
 import styles from './css/AddTagsModal.module.scss';
 import { useThemedIcon } from '../../hooks/ConditionalsHooks';
@@ -8,7 +8,7 @@ type Props = {
   confirmChoice: () => void,
   closeModal: () => void,
   tags: string[],
-  setArticleTags:  () => void,
+  setArticleTags: React.Dispatch<React.SetStateAction<string[]>>,
 }
 
 const AddTagsModal = ({isVisible, confirmChoice, closeModal, tags, setArticleTags}: Props) => {
@@ -17,30 +17,49 @@ const AddTagsModal = ({isVisible, confirmChoice, closeModal, tags, setArticleTag
   const [errorCategory, setErrorCategory] = useState<Errors | null>(null);
   const arrowHeadToLeftIconPath = useThemedIcon('arrow-head-to-left-icon.png');
   const arrowHeadToRightIconPath = useThemedIcon('arrow-head-to-right-icon.png');
+  const [selectedTagsList, setSelectedTagsList] = useState<string[]>([]);
+  const inputAddTag = useRef<HTMLInputElement | null>(null);
+
+  const handleSelectTag = (tagName: string) => {
+    setSelectedTagsList(prev => prev.includes(tagName) ? prev.filter(tag => tag !== tagName) : [...prev, tagName]);
+  }
+
+  const handleConfirmChoice = () => {
+    if(selectedTagsList.length > 0) {
+      setArticleTags(prev => prev.filter((tag) => !tags.includes(tag)));
+    }
+    
+    const modalInput = inputAddTag.current
+    
+    if(modalInput && modalInput.value.length > 0 && !selectedTagsList.includes(modalInput.value)) {
+      setArticleTags(prev => [...prev, modalInput.value])
+    }
+    closeModal();
+  }
 
   return (
     <div className={styles.modalContainer}>
       <div className={styles.addNewTagContainer}>
         <p>Add a new tag</p>
-        <input type="text" placeholder='New tag name...'/>
+        <input type="text" placeholder='New tag name...' ref={inputAddTag}/>
       </div>
 
-      <div className={styles.deleteTagsContainer}>
-        <p>Or select an existing one to delete:</p>
-        <div className={styles.selectTagsContainer}>
-          <button><img src={arrowHeadToLeftIconPath} alt="arrow-to-left-icon" /></button>
-          <div className={styles.existingTagsContainer}>
-            {tags.map((tag, index) => <span key={index} className={styles.tags}>{tag}</span>)}
+      {tags.length > 0 && 
+        <div className={styles.deleteTagsContainer}>
+          <p>Or select an existing one to delete:</p>
+          <div className={styles.selectTagsContainer}>
+            <div className={styles.existingTagsContainer}>
+              {tags.map((tag, index) => <span key={index} className={`${styles.tags} ${selectedTagsList.includes(tag) ? styles.selectedTag : ''}`} onClick={() => handleSelectTag(tag)}>{tag}</span>)}
+            </div>
           </div>
-          <button><img src={arrowHeadToRightIconPath} alt="arrow-to-right-icon" /></button>
         </div>
-      </div>
+      }
 
       {showErrorMessage && errorCategory && <ErrorMessage category={errorCategory}/>}
 
       <div className={styles.buttonsContainer}>
         <button className={styles.cancelButton} onClick={closeModal}>Cancel</button>
-        <button className={styles.confirmButton} onClick={confirmChoice}>Confirm</button>
+        <button className={styles.confirmButton} onClick={handleConfirmChoice}>Confirm</button>
       </div>
     </div>
   )
