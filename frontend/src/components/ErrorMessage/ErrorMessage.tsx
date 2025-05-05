6982f2382d6619a4b97b53921691137d4d9ab0d9
PB369 <pedrohbarros369@gmail.com>
2025-05-05 10:50:13 -0300

    ErrorMessage style under optimization

--- frontend/src/components/ErrorMessage/ErrorMessage.tsx
+++ frontend/src/components/ErrorMessage/ErrorMessage.tsx
@@ -3,11 +3,19 @@ import styles from './css/ErrorMessage.module.scss';
 export enum Errors {
   LoginValidation = "LoginValidation",
   ServerError = "ServerError",
+  TagAlreadyExist = "TagAlreadyExist",
+  TagsLimitReached = "TagLimitReached",
+  TagNameIsMissing = "TagNameIsMissing",
+  NoTagSettingsActionDefined = "NoTagSettingsActionDefined",
 }
 
 export const errorTexts: Record<Errors, string> = {
   [Errors.LoginValidation]: "Username or password is invalid.",
   [Errors.ServerError]: "This action couldn't be done due to server issues.",
+  [Errors.TagAlreadyExist]: "This name is already used in an existing tag.",
+  [Errors.TagsLimitReached]: "You have already reached the tag quantity limit",
+  [Errors.TagNameIsMissing]: "Add a name to the tag before confirm.",
+  [Errors.NoTagSettingsActionDefined]: "Add a name to a new  tag or select a created tag to delete before confirm.",
 }
 
 interface Props {
