e9f73f074094afeceb6f92d69f2b21949fb6fc32
PB369 <pedrohbarros369@gmail.com>
2025-05-13 09:38:34 -0300

    Style of the NotFound page is fully finished. Missing styles of Page messages system was added.

--- frontend/src/pages/NotFound/NotFound.tsx
+++ frontend/src/pages/NotFound/NotFound.tsx
@@ -1,16 +1,17 @@
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
 import './css/NotFound.module.scss';
+import styles from './css/NotFound.module.scss'
 
 const NotFound = () => {
   return (
-    <>
+    <div className={styles.notFoundPageContainer}>
       <Header/>
-      <div>
-        <p>Error 404 - This URL address doesn't exists.</p>
+      <div className={styles.pageMessageContainer}>
+        <h2 className={styles.pageMessage}>Error 404:<br/>This URL address doesn't exists.</h2>
       </div>
       <Footer/>
-    </>
+    </div>
   )
 }
 
