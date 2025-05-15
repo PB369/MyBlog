6b36f716219c0bc3639b371c521dfea89e04ed44
PB369 <pedrohbarros369@gmail.com>
2025-05-15 08:57:21 -0300

    The authentication error is fixed. The authentication system is fully functional

--- backend/authenticationController.js
+++ backend/authenticationController.js
@@ -8,16 +8,12 @@ const register = async (req, res) => {
     const { username, password } = req.body;
     try {
         const user = await getManager(username);
-        console.log("User: ", user);
         if(!user || !(await bcrypt.compare(password, user.password))) {
             return res.status(401).json({ error: "Invalid credentials" });
         }
-        console.log("Credentiais compatíveis");
         const token = jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: '2h' });
-        console.log("Token: ", token);
         res.json({ token });
-    } catch (err) {
-        console.log("ErroErro", err);
+    } catch {
         res.status(500).json({ error: "Login couldn't be done" });
     }
 }
